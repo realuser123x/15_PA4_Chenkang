58c25e9e604003f166b67d5bc1dca4dc04126e38
DionTanNotHere <64115210+DionTanNotHere@users.noreply.github.com>
2020-04-30 15:07:13 +0800

    PA4_Updated Dion
    by Dion

---
+++ PA4_Practice/15_PA4_Chenkang_task/Assets/Scripts/ChgToMain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChgToMain : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        {
+            StartCoroutine(WaitForIt(3.0F));
+        }
+        IEnumerator WaitForIt(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            SceneManager.LoadScene(1);
+        }
+    }
+}
